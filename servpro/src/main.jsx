23a7d67899fd1e5412e5948f7f03f1e170d43bb6
Rebeca-Alves <larissaalvessz00@gmail.com>
2024-09-29 17:56:14 -0300

    alteracoes iniciais
    instalação mui, rota para listagem-clientes, inicio da criacao tela listagem

--- servpro/src/main.jsx
+++ servpro/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login/Login'; 
 import Menu from './components/Menu/Sidebar'; 
+import ListarCliente from './pages/Listagem Clientes/ListarCliente';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
   {
     path: '/menu',
     element: <Menu />, 
+  },  
+  {
+    path: '/listarcliente',
+    element: <ListarCliente />, 
   },
 ]);
 
