87db69b5c0c137d10b5bd5ac06daeeb939e7657c
Rayane Souza <130245094+Rayane-Souza@users.noreply.github.com>
2024-10-15 17:16:48 -0300

    Inicializando Tela de Inicio

--- servpro/src/components/Menu/Sidebar.jsx
+++ servpro/src/components/Menu/Sidebar.jsx
@@ -1,65 +1,23 @@
-// import React, { useState } from 'react'; 
-// import './styles.css';
-// import logo from '../../assets/LogoServPro.png';
-// import MenuList from './MenuList';
-// import ListarCliente from '../../pages/ListagemClientes/ListarCliente'; 
-
-// import { AiOutlineMenu, AiOutlineBell, AiOutlineQuestionCircle, AiOutlineUser } from 'react-icons/ai'; 
-
-// function Sidebar() {
-//     const [collapsed, setCollapsed] = useState(false);
-
-//     const toggleSidebar = () => {
-//         setCollapsed(!collapsed);
-//     };
-
-//     return (
-//         <div className="layout">
-//             <aside className={`sidebar ${collapsed ? 'collapsed' : ''}`}>
-//                 <div className="logo-container">
-//                     <AiOutlineMenu className="menu-icon hamburger-icon" onClick={toggleSidebar} /> 
-//                     {!collapsed && <img src={logo} alt="Logo" className="logo" />}
-//                 </div>
-//                 <MenuList collapsed={collapsed} />
-//             </aside>
-//             <div className="main-layout">
-//                 <header className="header">
-//                     <div className="header-icons">
-//                         <AiOutlineBell className="header-icon" />
-//                         <AiOutlineQuestionCircle className="header-icon" />
-//                         <AiOutlineUser className="header-icon" />
-//                     </div>
-//                 </header>
-//                 <main className="content">
-//                 <ListarCliente /> 
-
-//                 </main>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Sidebar;
-
 import React, { useState } from 'react'; 
 import './styles.css';
 import logo from '../../assets/LogoServPro.png';
 import MenuList from './MenuList';
 import ListarCliente from '../../pages/ListagemClientes/ListarCliente'; 
-import ListagemTecnicos from '../../pages/ListagemTecnicos/ListarTecnico'; // Importar a listagem de técnicos
+import ListagemTecnicos from '../../pages/ListagemTecnicos/ListarTecnico';
+import TelaInicial from '../../pages/TelaInicial/TelaInicial'; 
 
 import { AiOutlineMenu, AiOutlineBell, AiOutlineQuestionCircle, AiOutlineUser } from 'react-icons/ai'; 
 
 function Sidebar() {
     const [collapsed, setCollapsed] = useState(false);
-    const [activeComponent, setActiveComponent] = useState('clientes'); // Estado para controlar o componente ativo
+    const [activeComponent, setActiveComponent] = useState('clientes'); 
 
     const toggleSidebar = () => {
         setCollapsed(!collapsed);
     };
 
     const handleMenuClick = (component) => {
-        setActiveComponent(component); // Atualiza o componente ativo
+        setActiveComponent(component); 
     };
 
     return (
@@ -69,7 +27,7 @@ function Sidebar() {
                     <AiOutlineMenu className="menu-icon hamburger-icon" onClick={toggleSidebar} /> 
                     {!collapsed && <img src={logo} alt="Logo" className="logo" />}
                 </div>
-                <MenuList collapsed={collapsed} onMenuClick={handleMenuClick} /> {/* Passa a função para o MenuList */}
+                <MenuList collapsed={collapsed} onMenuClick={handleMenuClick} /> 
             </aside>
             <div className="main-layout">
                 <header className="header">
@@ -80,8 +38,9 @@ function Sidebar() {
                     </div>
                 </header>
                 <main className="content">
-                    {activeComponent === 'clientes' && <ListarCliente />} {/* Renderiza ListarCliente */}
-                    {activeComponent === 'tecnico' && <ListagemTecnicos />} {/* Renderiza ListagemTecnicos */}
+                    {activeComponent === 'clientes' && <ListarCliente />} 
+                    {activeComponent === 'tecnico' && <ListagemTecnicos />}
+                    {activeComponent === 'inicio' && <TelaInicial />} 
                 </main>
             </div>
         </div>
