1a1e864e3e6da14f98cf5f827d4bb46139f4eab7
Paloma Cezário <147927123+PalomaCezario@users.noreply.github.com>
2024-11-10 19:30:55 -0300

    ajuste no perfil do usuario

--- servpro/src/main.jsx
+++ servpro/src/main.jsx
@@ -43,7 +43,8 @@ const router = createBrowserRouter([
   {
     path: '/perfildousuario',
     element: <PerfilDoUsuario />, 
-
+  },
+  {
     path: '/cadastrotecnico',
     element: <CadastroTecnico />, 
   },
