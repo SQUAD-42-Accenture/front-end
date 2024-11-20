08117083accb185367b896ac05faa5f83fbeb645
Rayane-Souza <rayannesilvasouza@outlook.com>
2024-11-20 14:51:26 -0300

    ListaDeOrdens

--- servpro/src/pages/ListagemOrdens/ListarOrdens.jsx
+++ servpro/src/pages/ListagemOrdens/ListarOrdens.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Button, IconButton, Pagination } from '@mui/material';
 import { Download } from '@mui/icons-material';
 import SearchIcon from '@mui/icons-material/Search';
 import AddIcon from '@mui/icons-material/PersonAdd';
 import { Link } from 'react-router-dom';
-
-const ordensServico = [
-  { ordem: 34567, cliente: 'Maria Costa', data: '2023-10-01', equipamento: 'Máquina 1', tecnico: 'João', orcamento: 'R$ 100,00', status: 'Concluído' },
-  { ordem: 45678, cliente: 'Carlos Silva', data: '2023-10-03', equipamento: 'Máquina 3', tecnico: 'Maria', orcamento: 'R$ 200,00', status: 'Pendente' },
-  { ordem: 56789, cliente: 'Ana Clara', data: '2023-10-04', equipamento: 'Máquina 4', tecnico: 'Pedro', orcamento: 'R$ 250,00', status: 'Cancelado' },
-  { ordem: 67890, cliente: 'Lucas Mendes', data: '2023-10-05', equipamento: 'Máquina 5', tecnico: 'Ricardo', orcamento: 'R$ 300,00', status: 'Aberto' },
-  { ordem: 34567, cliente: 'Maria Costa', data: '2023-10-01', equipamento: 'Máquina 1', tecnico: 'João', orcamento: 'R$ 100,00', status: 'Concluído' },
-  { ordem: 56789, cliente: 'Ana Clara', data: '2023-10-04', equipamento: 'Máquina 4', tecnico: 'Pedro', orcamento: 'R$ 250,00', status: 'Cancelado' },
-];
+import axios from 'axios';
 
 function ListagemOrdensServico() {
+  const [ordensServico, setOrdensServico] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5238/api/OrdemDeServico')
+      .then((response) => {
+        setOrdensServico(response.data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar ordens de serviço", error);
+      });
+  }, []);
+
+  const filteredOrdens = ordensServico.filter((ordem) =>
+    ordem.Descricao.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+ 
+  const downloadPDF = (OrdemDeServicoId) => {
+    if (OrdemDeServicoId) {
+      const pdfUrl = `http://localhost:5238/api/OrdemDeServico/${OrdemDeServicoId}/gerar-pdf`; 
+      console.log('Gerando PDF para a ordem de serviço com ID:', ordemId);
+      window.open(pdfUrl, '_blank'); 
+    } else {
+      console.error('ID da ordem de serviço não encontrado!');
+    }
+  };
+
   return (
-    <div style={{ padding: '20px', marginTop: '10px' }}> 
+    <div style={{ padding: '20px', marginTop: '10px' }}>
       <div style={{
         backgroundColor: 'white',
         boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
@@ -38,6 +58,8 @@ function ListagemOrdensServico() {
             variant="outlined"
             placeholder="Buscar Ordem"
             size="small"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
             InputProps={{
               startAdornment: <SearchIcon />,
             }}
@@ -60,42 +82,42 @@ function ListagemOrdensServico() {
       </div>
 
       <TableContainer component={Paper} style={{ maxWidth: '100%', margin: '0 auto', marginTop: '20px' }}>
-        <Table aria-label="simple table">
+        <Table aria-label="Ordens de Serviço">
           <TableHead>
             <TableRow>
-              <TableCell>Ordem</TableCell>
+              <TableCell>Número da OS</TableCell> 
               <TableCell>Cliente</TableCell>
-              <TableCell>Data da Solicitação</TableCell>
+              <TableCell>Data de Abertura</TableCell>
               <TableCell>Equipamento</TableCell>
               <TableCell>Técnico</TableCell>
-              <TableCell>Orçamento</TableCell>
+              <TableCell>Pagamento</TableCell>
               <TableCell>Status</TableCell>
               <TableCell>Ações</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {ordensServico.map((ordem) => (
-              <TableRow key={ordem.ordem}>
-                <TableCell>{ordem.ordem}</TableCell>
-                <TableCell>{ordem.cliente}</TableCell>
-                <TableCell>{ordem.data}</TableCell>
-                <TableCell>{ordem.equipamento}</TableCell>
-                <TableCell>{ordem.tecnico}</TableCell>
-                <TableCell>{ordem.orcamento}</TableCell>
+            {filteredOrdens.map((ordem) => (
+              <TableRow key={ordem.OrdemDeServicoId}>
+                <TableCell>{ordem.OrdemDeServicoId}</TableCell> 
+                <TableCell>{ordem.ClienteCPF}</TableCell>
+                <TableCell>{new Date(ordem.dataAbertura).toLocaleDateString()}</TableCell> 
+                <TableCell>{ordem.SerialEquipamento}</TableCell> 
+                <TableCell>{ordem.TecnicoCPF}</TableCell>
+                <TableCell>{ordem.MetodoPagamento}</TableCell>
                 <TableCell>
-                  <Button 
-                    variant="contained" 
-                    style={{ 
-                      backgroundColor: getStatusColor(ordem.status), 
-                      width: '120px', 
-                      textAlign: 'center' 
+                  <Button
+                    variant="contained"
+                    style={{
+                      backgroundColor: getStatusColor(ordem.Status),
+                      width: '120px',
+                      textAlign: 'center'
                     }}
                   >
-                    {ordem.status}
+                    {ordem.Status}
                   </Button>
                 </TableCell>
                 <TableCell>
-                  <IconButton color="primary" onClick={() => downloadPDF(ordem.ordem)}>
+                  <IconButton color="primary" onClick={() => downloadPDF(ordem.OrdemDeServicoId)}>
                     <Download />
                   </IconButton>
                 </TableCell>
@@ -106,7 +128,7 @@ function ListagemOrdensServico() {
       </TableContainer>
 
       <Pagination
-        count={10}
+        count={Math.ceil(filteredOrdens.length / 10)}
         variant="outlined"
         shape="rounded"
         color="primary"
@@ -124,22 +146,18 @@ const getStatusColor = (status) => {
   switch (status) {
     case 'Concluído':
       return 'green';
-    case 'Em andamento':
+    case 'Em Andamento':
       return 'blue';
     case 'Pendente':
       return 'orange';
-    case 'Cancelado':
+    case 'Cancelada':
       return 'red';
-    case 'Aberto':
-      return 'gray';
     default:
       return 'transparent';
   }
 };
 
-const downloadPDF = (ordem) => {
-  const pdfUrl = `http://localhost:5238/api/OrdemDeServico/${ordem}/gerar-pdf`;
-  window.open(pdfUrl, '_blank');
-};
-
 export default ListagemOrdensServico;
+
+
+
